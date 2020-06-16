9d4663e9f0feadc6cfec88c032a04bc9ad371a42
Wojtek Swiderski <wswiders@uwaterloo.ca>
2020-06-15 23:33:21 -0700

    More minor tweaks

--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -1,52 +1,14 @@
 import React from "react";
-import Query from "../Query";
 import { Link } from "react-router-dom";
-
-import CATEGORIES_QUERY from "../../queries/category/categories";
 
 const Nav = () => {
   return (
-    <div>
-      <Query query={CATEGORIES_QUERY} id={null}>
-        {({ data: { categories } }) => {
-          return (
-            <div>
-              <nav className="uk-navbar-container" data-uk-navbar>
-                <div className="uk-navbar-left">
-                  <ul className="uk-navbar-nav">
-                    <li>
-                      <Link to="/">Strapi Blog</Link>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="uk-navbar-center">
-                  <h3 className="logo-text">
-                    <Link to="/">wojtechnology</Link>
-                  </h3>
-                </div>
-
-                <div className="uk-navbar-right">
-                  <ul className="uk-navbar-nav">
-                    {categories.map((category, i) => {
-                      return (
-                        <li key={category.id}>
-                          <Link
-                            to={`/category/${category.id}`}
-                            className="uk-link-reset"
-                          >
-                            {category.name}
-                          </Link>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
-              </nav>
-            </div>
-          );
-        }}
-      </Query>
+    <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
+      <nav className="uk-navbar-container" data-uk-navbar>
+        <div className="uk-navbar-center">
+          <Link className="logo-text" to="/">wojtechnology</Link>
+        </div>
+      </nav>
     </div>
   );
 };
